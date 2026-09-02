9fffc5f0fca1f94dde88d11e3273a3c03a6b94c6
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2026-09-02 16:40:24 -0400

    chapter 2

---
+++ Chapter2/main.py
@@ -0,0 +1,64 @@
+import random
+
+
+name = input("Enter your name")
+favorite_number = float(input("Enter your favorite number"))
+
+print(favorite_number * 10)
+
+print(f'Hi {name}')
+
+print('hello world')
+
+name = 'Eric Charnesky'
+
+first_letter = 'E'
+second_letter = 'r'
+third_letter = 'i'
+fourth_letter = 'c'
+
+first_letter = chr(ord(first_letter) + 1)
+second_letter = chr(ord(second_letter) + 1)
+third_letter = chr(ord(third_letter) + 1)
+fourth_letter = chr(ord(fourth_letter) + 1)
+
+print(f'{first_letter}{second_letter}{third_letter}{fourth_letter}')
+
+print(name)
+
+age = 39
+
+print(age)
+
+age = age + 1
+
+print(age)
+
+age = age * 2 / 4 + 7 * 3
+
+print(age)
+
+
+number = 11 / 3
+
+number = number * 1.5 * 2
+print(number )
+
+#random.seed(10)
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+
+print(name,"is", age-2,"years old")
+print(f'{name} is {age-2} years old')
+
+
+
+# 10^2   10^1   10^0
+
+# 2^2  2^1 2^0
